ab398b03c6d9dfcbb91a722e8acd0fd2bad7526a
Danny Wilson <dannywilson32@gmail.com>
2013-11-30 20:26:45 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitness::Application.config.secret_key_base = 'c9ce88e0cb83c0c34cd35ca88b544c2411359da6d599093ec39d10aa6c523db7d147c6235f65ee9a02591de51de82dec4674ade99fb79c23921e34ddbf07dccc'
