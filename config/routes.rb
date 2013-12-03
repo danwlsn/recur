c195c08a48cba60014fb2e3464761857d6160142
Danny Wilson <dannywilson32@gmail.com>
2013-12-03 18:46:06 +0000

    cheap user sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Fitness::Application.routes.draw do
+
+  resources :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +43,7 @@ Fitness::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
