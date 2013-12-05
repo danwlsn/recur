948abed64565132061a306e8cfd450c835f533e5
Danny Wilson <dannywilson32@gmail.com>
2013-12-05 12:17:36 +0000

    user update

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,12 @@ class User < ActiveRecord::Base
 	validates :password, length: { minimum: 6 }
 
 	 def User.new_remember_token
-    SecureRandom.urlsafe_base64
-  end
+		SecureRandom.urlsafe_base64
+	end
 
-  def User.encrypt(token)
-    Digest::SHA1.hexdigest(token.to_s)
-  end
+	def User.encrypt(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
 
 	private
 	def create_remember_token
