76a6966bbbd899aa0aa80f58c566f9b7af1ee131
Danny Wilson <dannywilson32@gmail.com>
2014-02-01 20:09:49 +0000

    Post rake for options

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140123145626) do
+ActiveRecord::Schema.define(version: 20140201200821) do
 
   create_table "current_weights", force: true do |t|
     t.integer  "user_id"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20140123145626) do
   create_table "goal_weights", force: true do |t|
     t.integer  "user_id"
     t.integer  "weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "options", force: true do |t|
+    t.integer  "user_id"
+    t.string   "weight"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
