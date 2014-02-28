0186feb124a8064cfb52f52a578194a3a91c9a49
Danny Wilson <dannywilson32@gmail.com>
2014-02-28 19:07:37 +0000

    Double highcharts?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,4 @@
 // = require jquery
 // = require jquery_ujs
 // = require turbolinks
-// = require highcharts/highcharts
-// = require highcharts/highcharts-more
-// = require highcharts/highstock
 // = require_tree .
