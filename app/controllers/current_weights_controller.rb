5dc7e5c7251fb71b5e9890d1b7825bfe169a7bf4
Danny Wilson <dannywilson32@gmail.com>
2014-03-01 14:12:28 +0000

    Fixed flash on weights. Updates on ajax and uses partial

--- app/controllers/current_weights_controller.rb
+++ app/controllers/current_weights_controller.rb
@@ -5,8 +5,8 @@ class CurrentWeightsController < ApplicationController
 		# Build new current weight for user
 		@current_weight = current_user.current_weights.build(weight_params)
 		if @current_weight.save # If saved
-			flash[:success] = "Weight updated"
 			respond_to do |format|
+				flash[:success] = "Weight updated"
 				format.html { redirect_to current_user }
 				format.js
 			end
