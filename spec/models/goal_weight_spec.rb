8f02660ba59287cd800d433df21562893f3284f3
Danny Wilson <dannywilson32@gmail.com>
2014-03-29 15:54:24 +0000

    Goal weight test

---
+++ spec/models/goal_weight_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe GoalWeight do
+	before { @weight = GoalWeight.new(weight: 200) }
+
+	subject { @weight }
+
+	it { should respond_to(:weight) }
+
+	it { should be_valid}
+
+	describe "when weight is not present" do
+		before { @weight.weight = "" }
+		it { should_not be_valid }
+	end
+
+	describe "when weight is not a number" do
+		before { @weight.weight = "foobar" }
+		it { should_not be_valid }
+	end
+end
