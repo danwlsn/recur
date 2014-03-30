187c14c25d69fbc9bbd42fb3729f3058a2a60b87
Danny Wilson <dannywilson32@gmail.com>
2014-03-30 19:59:04 +0100

    Factory girl and osme more tests

--- spec/requests/user_page_spec.rb
+++ spec/requests/user_page_spec.rb
@@ -4,21 +4,33 @@ describe "User pages" do
 
 	subject { page }
 
-	describe "signup page" do
+	describe "signup" do
 		before { visit signup_path }
-		it { should have_content('Welcome') }
+
+		let(:submit) { "Sign me up" }
+
+		describe "with invalid information" do
+			it "should not create a user" do
+				expect { click_button submit }.not_to change(User, :count)
+			end
+		end
+
+		describe "with valid information" do
+			before do
+				fill_in "Full name",         with: "Example User"
+				fill_in "Email",        with: "user@example.com"
+				fill_in "Secret password",     with: "foobar"
+				fill_in "Confirm secret password", with: "foobar"
+			end
+
+			it "should create a user" do
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+		end
 	end
 
 	describe "signin page" do
 		before { visit signin_path }
 		it { should have_content('Welcome back') }
 	end
-
-	describe "profile page" do
-		before { @user = User.new(id: 1, name: "Foo Bar", email: "foo@bar.com", password: "foobar", password_confirmation: "foobar")}
-		before { visit user_path(@user) }
-
-		it { should have_content(@user.name) }
-	end
-
 end
